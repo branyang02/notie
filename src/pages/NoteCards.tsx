7e7fb0d4e19a1a7d0c450eb210908cf01355417d
Brandon Yang <107154811+branyang02@users.noreply.github.com>
2024-05-06 09:32:11 -0400

    moved notes folder to .

--- src/pages/NoteCards.tsx
+++ src/pages/NoteCards.tsx
@@ -2,7 +2,7 @@ import { Card, Heading, majorScale, Pane, Text } from 'evergreen-ui';
 import { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 
-const modules = import.meta.glob('../notes/*.md', { as: 'raw' });
+const modules = import.meta.glob('../../notes/*.md', { as: 'raw' });
 
 interface NotesMetaData {
   title?: string;
