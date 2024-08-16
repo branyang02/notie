d523a12aef996a935a433adc2d93e341493a5b5d
Brandon Yang <107154811+branyang02@users.noreply.github.com>
2024-08-16 11:35:19 -0400

    add addition import

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
 export { default as Notie } from "./components/Notie";
 export type { NotieConfig, NotieThemes } from "./config/NotieConfig";
 export type { NotieProps } from "./components/Notie";
+export type { Theme } from "./config/NotieConfig";
